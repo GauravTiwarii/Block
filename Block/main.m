891c7087d904c70d63adfb9532ac51689a12bb26
Love <Love@love.local>
2016-06-04 01:40:01 +0530

    Initial Commit

---
+++ Block/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Block
+//
+//  Created by Love on 6/4/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
